9ca7c28851cfe9877f40feeecc76ab9c0fe62a01
Tayyab Khurram <tayyabsheikh.pakistan@gmail.com>
2025-11-16 16:20:17 +0500

    Modify manifest.json and edit URL in test the model file

--- test_the_model.py
+++ test_the_model.py
@@ -24,8 +24,10 @@ except FileNotFoundError:
 # Try some obvious ones first!
 # unknown_url = "https://multidisciplinary-amount-743935.framer.app/"
 # unknown_url = "https://gahmg.puniro.cfd/"
-unknown_url = "https://paperclip.pk/"
+unknown_url = "https://doceno.malahim.dev/login"
+# unknown_url = "https://paperclip.pk/"
 # unknown_url = "https://www.apponix.com/cloud-computing-training-course-in-Lahore/"
+# unknown_url = "https://coinbasellcus.com/"
 
 
 print(f"\nTesting URL: {unknown_url}")
